2eb0d0c0d20acd4fb4e1d67d5a8ad20d22c61acb
Kai M. Hall <66092880+micale24@users.noreply.github.com>
2021-12-08 16:38:33 -0500

    deleted unnecessary code

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,6 @@
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 var text = d3.select("#text");
-//Console.log the weather data from data.js
-// console.log(moves.keys(1));
-
-// data.forEach((move) => {
-//     var row = tbody.append("tr");
-//     Object.entries(move).forEach(([key, value]) => {
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
-
-
-
-// function flow_random() {
-//     var moveList = []
-//     var i = 0
-//     while (i in Range(inputValue)){
-//         move_1 = string(Math.floor(Math.random() * (max - min + 1) ) + min);
-//         flow_1 = data.filter(move_1)
-//         //moveList.append(flow_1)
-//         i++
-//         var newRows = d3.select("tbody");
-//         var newRowstext = d3.select("#text");
-//         newRows.html("")
-//         moveList.forEach((flow_1) => {
-//           var newRowstext = newRows.append("tr");
-//           Object.entries(flow_1).forEach(([key, value]) =>{
-//             var cell = newRowstext.append("td");
-//             cell.text(value);
-//           });
-//         });
-//     }
-//     return moveList
-// }
-
-// console.log(move_1)
-// Assign the data from `data.js` to a descriptive variable
-// var moves = data;
 
 // Select the button
 var button = d3.select("#button");
@@ -86,27 +48,9 @@ function runEnter() {
           var newRowstext = newRows.append("tr");
           var cell = newRowstext.append("td")
           cell.text(move)
-        //    Object.entries(move).forEach(([key, value]) =>{
-        //     console.log(key)
-        //     console.log(value)
-            // var cell = newRowstext.append("tr");
-            // cell.text(value)
-        //   });
+      
         });
         i++
     }
     
-  //   var filteredData = filterDate.filter(dateInput => dateInput.datetime === inputValue);
-  
-  // remove the contents and update with user input date and data
-    //   var newRows = d3.select("tbody");
-    //   var newRowstext = d3.select("#text");
-    //   newRows.html("")
-    //   moveList.forEach((moveFlow) => {
-    //     var newRowstext = newRows.append("tr");
-    //     Object.entries(filterweather).forEach(([key, value]) =>{
-    //       var cell = newRowstext.append("td");
-    //       cell.text(value);
-    //     });
-    //   });
 };
